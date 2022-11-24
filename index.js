c74359a394f31a1446509607796e08b587eaa860
emon <emondas457@gmail.com>
2022-11-25 00:22:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ljdbc6c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      const phoneCategoriCollection = client.db("sellphone").collection("phoneCategori");
+
+        app.get('/phoneCategori', async(req, res)=>{
+            const phoneCate = await phoneCategoriCollection.find({}).toArray();
+            res.send(phoneCate)
+        })
+  
+    } finally {
+  
+    }
+  }
+  run().catch(console.dir);
+  
+
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+})
