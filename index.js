205f3de9fbb7088999a6f5c759d6f6c2ec84754b
emon <emondas457@gmail.com>
2022-11-25 00:55:49 +0600

    get data by id

--- index.js
+++ index.js
@@ -18,19 +18,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-      const phoneCategoriCollection = client.db("sellphone").collection("phoneCategori");
+        const phoneCategoriCollection = client.db("sellphone").collection("phoneCategori");
 
-        app.get('/phoneCategori', async(req, res)=>{
+        app.get('/phoneCategori', async (req, res) => {
             const phoneCate = await phoneCategoriCollection.find({}).toArray();
             res.send(phoneCate)
         })
-  
+
+        app.get('/phoneCategori/:id', async (req, res) => {
+            const query = {};
+            const service = phoneCategoriCollection.find(query);
+            const newservice = await service.toArray();
+            const findServiceById = newservice.find(getService => getService._id == req.params.id)
+            res.send(findServiceById);
+          })
+
     } finally {
-  
+
     }
-  }
-  run().catch(console.dir);
-  
+}
+run().catch(console.dir);
+
 
 
 app.listen(port, () => {
