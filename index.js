739394a56c76c8bd8b93345a8b610a2acde6de83
emon <emondas457@gmail.com>
2022-11-26 12:15:27 +0600

    get user by email

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ async function run() {
         const phoneCategoriCollection = client.db("sellphone").collection("phoneCategori");
         const userCollection = client.db("sellphone").collection("user");
         const phonesCollection = client.db("sellphone").collection("phones");
+        const brandCollection = client.db("sellphone").collection("brand");
+
         app.get('/phoneCategori', async (req, res) => {
             const phoneCate = await phoneCategoriCollection.find({}).toArray();
             res.send(phoneCate)
@@ -33,20 +35,50 @@ async function run() {
             const findServiceById = newservice.find(getService => getService._id == req.params.id)
             res.send(findServiceById);
         })
+
+        // get phones by user email
+        app.get('/getphones', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = phonesCollection.find(query);
+            const phones = await cursor.toArray();
+            res.send(phones)
+        })
+
+        // // get phones by user email
+        // app.get('/phones/:email', async (req, res) => {
+        //     const query = {};
+        //     const service = phonesCollection.find(query);
+        //     const newservice = await service.toArray();
+        //     const findServiceById = newservice.filter(getService => getService.email === req.params.email)
+        //     res.send(findServiceById);
+        // })
+
+
         // get phones by brand
         app.get('/phones/:brand', async (req, res) => {
             const query = {};
-            const service = phoneCategoriCollection.find(query);
+            const service = phonesCollection.find(query);
             const newphone = await service.toArray();
-            const phones = newphone.find(getphone => getphone.brand == req.params.brand)
+            const phones = newphone.filter(getphone => getphone.brand === req.params.brand)
             res.send(phones);
         })
 
         // post phones
-        app.post('/phones',async(req,res)=>{
+        app.post('/phones', async (req, res) => {
             const phone = req.body;
             const phones = await phonesCollection.insertOne(phone);
             res.send(phones);
+        })
+
+        // get brand
+        app.get('/brand', async (req, res) => {
+            const brand = await brandCollection.find({}).toArray();
+            res.send(brand);
         })
 
         app.post('/users', async (req, res) => {
